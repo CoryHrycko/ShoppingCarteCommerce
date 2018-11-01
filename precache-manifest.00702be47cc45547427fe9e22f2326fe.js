0a255e7ecd7c6d58332d318e12b8400d664ffe61
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-11-01 01:16:25 -0400

    Updates

---
+++ precache-manifest.00702be47cc45547427fe9e22f2326fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a3b8c36df38b25c42183fd5656f4642",
+    "url": "/ShoppingCarteCommerce/static/media/ian-keefe.2a3b8c36.jpg"
+  },
+  {
+    "revision": "1172e801588b35fe58f6",
+    "url": "/ShoppingCarteCommerce/static/js/runtime~main.1172e801.js"
+  },
+  {
+    "revision": "27d02487f3a9f4795a6e",
+    "url": "/ShoppingCarteCommerce/static/js/main.27d02487.chunk.js"
+  },
+  {
+    "revision": "d6d80534679ccf523597",
+    "url": "/ShoppingCarteCommerce/static/js/1.d6d80534.chunk.js"
+  },
+  {
+    "revision": "27d02487f3a9f4795a6e",
+    "url": "/ShoppingCarteCommerce/static/css/main.e828015a.chunk.css"
+  },
+  {
+    "revision": "d975a1cd766120928be84656fbc34173",
+    "url": "/ShoppingCarteCommerce/index.html"
+  }
+];
