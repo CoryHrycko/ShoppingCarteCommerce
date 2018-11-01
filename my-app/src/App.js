5996525f4c50329e37cfcc21deee489e7df3ba54
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-31 23:09:56 -0400

    messing with the header as an introduction to the page.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
          <h2>Welcome</h2>
          </div>
         <div className="App-intro">
